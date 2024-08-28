3fa706f58244de6a28f374961197138f0eab8256
adsoog <iadso.ao@gmail.com>
2024-08-27 22:06:59 -0500

    ready for render with superuser

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Install dependencies
 pip install -r requirements.txt
 
 # Convert static asset files
@@ -11,3 +11,15 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
+# Create a superuser if it doesn't exist
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = "${DJANGO_SUPERUSER_USERNAME:-admin}"
+email = "${DJANGO_SUPERUSER_EMAIL:-admin@example.com}"
+password = "${DJANGO_SUPERUSER_PASSWORD:-password}"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+EOF
